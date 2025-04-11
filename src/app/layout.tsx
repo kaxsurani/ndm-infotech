7f91708e7153fb48fd330c95cb157deeed748f87
Ridham <ridhamavaiya1999@gmail.com>
2025-04-11 09:58:44 +0530

    Add verification code for search console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="sqiBdWjmety7GKqaCfVdYMO6SjmMvyrlSOSH1stcOLk" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
